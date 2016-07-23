745cd420b47b902741889fa08fa5e8a2cf3d1e01
je0489 <je0489@naver.com>
2016-07-23 22:38:57 +0900

    단골매장 CRD완료 + new 정보 재전송

--- src/main/java/ttowang/service/JieunService.java
+++ src/main/java/ttowang/service/JieunService.java
@@ -1,25 +1,47 @@
-package ttowang.service;
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE mapper PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN" "http://mybatis.org/dtd/mybatis-3-mapper.dtd">
 
-import java.util.List;
-import java.util.Map;
+<mapper namespace="jieun">
 
-import javax.annotation.Resource;
+<!-- 단골 가맹점 리스트 -->
+	<select id="selectBoardList" parameterType="hashmap" resultType="hashmap">
+        <![CDATA[
+        	SELECT
+     			*
+    		FROM
+    			BUSINESS B,MEMBERSHIP M
+    		WHERE 
+    			M.businessID = B.businessID    			
+        ]]>
+    </select>
+    
+    <select id="selectCheckMembership" parameterType="hashmap" resultType="hashmap">
+        <![CDATA[
+        	SELECT
+     			*
+    		FROM
+    			MEMBERSHIP M		
+        ]]>
+    </select>
+    
+    <insert id="insertMyBusiness" parameterType="hashmap">
+    	<![CDATA[
+    		INSERT
+    			INTO MEMBERSHIP
+    		VALUES
+    			( #{USERID} , #{BUSINESSID} )
+    	]]>
+    </insert>
+    
+    <delete id="deleteMyBusiness" parameterType="hashmap">
+    	<![CDATA[
+    		DELETE FROM
+    			MEMBERSHIP
+    		WHERE
+    			userID = #{USERID}
+			AND
+				businessID = #{BUSINESSID}
+    	]]>
+    </delete>
+</mapper>
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import ttowang.dao.JieunDao;
-
-@Service("JieunService")
-public class JieunService {
-	
-	@Resource(name="JieunDao")
-	private JieunDao dao;
-
-	
-	
-
-
-	
-
-}
