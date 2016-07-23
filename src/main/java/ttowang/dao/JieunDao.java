2a29f15950d446499ecac6fff1d46ff691a1ce84
je0489 <je0489@naver.com>
2016-07-23 22:34:40 +0900

    Update JieunDao.java
    단골매장 CRU완료 + new 정보 재전송

--- src/main/java/ttowang/dao/JieunDao.java
+++ src/main/java/ttowang/dao/JieunDao.java
@@ -7,10 +7,24 @@ import org.springframework.stereotype.Repository;
 
 import ttowang.abst.dao.abDAO;
 
-@SuppressWarnings("unchecked")
 @Repository("JieunDao")
 public class JieunDao extends abDAO{
 
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectMyBusinessList(Map<String, Object> map) throws Exception{
+        return (List<Map<String, Object>>)selectList("jieun.selectBoardList", map);
+    }
 
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectCheckMembership(Map<String, Object> map) {
+		return (List<Map<String, Object>>)selectList("jieun.selectCheckMembership", map);
+	}
 
+	public void insertMyBusiness(Map<String, Object> map) {
+		insert("jieun.insertMyBusiness",map);
+	}
+
+	public void deleteMyBusiness(Map<String, Object> map) {
+		delete("jieun.deleteMyBusiness",map);
+	}
 }
