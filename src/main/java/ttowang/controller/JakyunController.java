3aca71bb21596240c0c7aac6d4560215f3495eb6
jakyun <olrw02@naver.com>
2016-07-23 22:20:57 +0900

    정회원 회원가입

--- src/main/java/ttowang/controller/JakyunController.java
+++ src/main/java/ttowang/controller/JakyunController.java
@@ -1,6 +1,8 @@
 package ttowang.controller;
 
 import java.util.HashMap;
+
+
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +23,37 @@ public class JakyunController {
 
 	@Resource(name = "JakyunService")
 	private JakyunService service;
+	
+	@RequestMapping("/insertUser.do")
+	public void insertUser(HttpServletRequest request) throws Exception {
+		Map<String, Object> map = new HashMap<String, Object>();
 
+		String tel = request.getParameter("edt_tel");
+		String name = request.getParameter("edt_name");
+		String birth = request.getParameter("edt_birth");
+		String gender = request.getParameter("edt_gender");
+		String email = request.getParameter("edt_email");
+		
+		map.put("tel", tel);
+		map.put("name", name);
+		map.put("birth", birth);
+		map.put("gender", gender);
+		map.put("email", email);
+
+		// userCode=1 -> 정회원
+		// userCode=0 -> 준회원
+		
+		//준회원도 아닌 경우
+		if(service.checkUser(map) == null) {
+			map.put("code", 1);
+			service.insertUser(map);
+		}
+		
+		//준회원인 경우
+		else {
+			service.updateUser(map);
+		}
+	}
+	
 	
 }
