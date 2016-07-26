fd2ec5d6ce1f9439f7b8f229ab3277860946efe9
jakyun <olrw02@naver.com>
2016-07-27 00:07:40 +0900

    checkUser

--- src/main/java/ttowang/controller/JakyunController.java
+++ src/main/java/ttowang/controller/JakyunController.java
@@ -24,6 +24,26 @@ public class JakyunController {
 	@Resource(name = "JakyunService")
 	private JakyunService service;
 	
+	@RequestMapping(value="/checkUser.do")
+	public ModelAndView checkUser(Map<String, Object> commandMap, HttpServletRequest request)throws Exception {		
+		try{
+			ModelAndView mv = new ModelAndView("jsonView");
+			
+			String userTel = request.getParameter("userTel");
+			commandMap.put("userTel", userTel);
+			
+			List<Map<String, Object>> list = service.checkUser(commandMap);
+			mv.addObject("checkUser", list);
+			
+			System.out.println("checkUser 성공");
+			return mv;
+					
+		}catch(Exception e){
+			System.out.println("checkUser 실패");
+			return null;
+		}
+	}
+	
 	@RequestMapping("/insertUser.do")
 	public void insertUser(HttpServletRequest request) throws Exception {
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -44,7 +64,7 @@ public class JakyunController {
 		// userCode=0 -> 준회원
 		
 		//준회원도 아닌 경우
-		if(service.checkUser(map) == null) {
+		if(service.checkCode(map) == null) {
 			map.put("userCode", 1);
 			service.insertUser(map);
 		}
@@ -53,6 +73,5 @@ public class JakyunController {
 		else {
 			service.updateUser(map);
 		}
-	}
-		
+	}	
 }
