74618244913a0ecf44b30404b12129a16a304c63
jakyun <olrw02@naver.com>
2016-09-10 16:04:09 +0900

    스템프적립 checkTel

--- src/main/java/ttowang/controller/JakyunController.java
+++ src/main/java/ttowang/controller/JakyunController.java
@@ -2,7 +2,6 @@ package ttowang.controller;
 
 import java.util.HashMap;
 
-
 import java.util.List;
 import java.util.Map;
 
@@ -24,12 +23,15 @@ public class JakyunController {
 	@Resource(name = "JakyunService")
 	private JakyunService service;
 	
+	//정회원은 회원가입 없이 바로 어플이용
+	//준회원이거나 회원이 아닌 사람들은 회원가입
 	@RequestMapping(value="/checkUser.do")
 	public ModelAndView checkUser(Map<String, Object> commandMap, HttpServletRequest request)throws Exception {		
 		try{
 			ModelAndView mv = new ModelAndView("jsonView");
 			
 			String userTel = request.getParameter("userTel");
+			
 			commandMap.put("userTel", userTel);
 			
 			List<Map<String, Object>> list = service.checkUser(commandMap);
@@ -44,6 +46,8 @@ public class JakyunController {
 		}
 	}
 	
+	//준회원은 있던 번호에 userName, userBirth, userGender, userCode, userEmail 업데이트
+	//회원이 아닌 사람들은 추가(userTel, userName, userBirth, userGender, userCode, userEmail)
 	@RequestMapping("/insertUser.do")
 	public void insertUser(HttpServletRequest request) throws Exception {
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -61,19 +65,21 @@ public class JakyunController {
 		map.put("userEmail", userEmail);
 
 		// userCode=1 -> 정회원
-		// userCode=0 -> 준회원
+		// userCode=4 -> 준회원
 		
-		//준회원도 아닌 경우
+		//회원이 아닌 경우
 		if(service.checkCode(map) == null) {
 			map.put("userCode", 1);
 			service.insertUser(map);
+			System.out.println("insertUser 성공");
 		}
 		
 		//준회원인 경우
 		else {
 			service.updateUser(map);
+			System.out.println("updateUser 성공");
 		}
-	}
+	}	
 	
 	@RequestMapping("/myInfoEdit.do")
 	public void myIndoEdit(HttpServletRequest request) throws Exception {
@@ -94,5 +100,30 @@ public class JakyunController {
 		System.out.println("myInfoEdit 성공");
 		
 		service.myInfoEdit(map);
+	}
+	
+	@RequestMapping(value="/checkTel.do")
+	public ModelAndView checkTel(Map<String, Object> commandMap, HttpServletRequest request)throws Exception {		
+		try{
+			ModelAndView mv = new ModelAndView("jsonView");
+			
+			String tel = request.getParameter("tel");
+			
+			commandMap.put("tel", tel);
+			
+			System.out.println(tel);
+			
+			List<Map<String, Object>> list = service.checkTel(commandMap);
+			mv.addObject("checkTel", list);
+			
+			System.out.println(list);
+			
+			System.out.println("checkTel 성공");
+			return mv;
+					
+		}catch(Exception e){
+			System.out.println("checkTel 실패");
+			return null;
+		}
 	}
 }
